ffab69e9028e01947beda6a95b9de05c7ab07d47
Matias Barcenas <MPX.4132@Gmail.com>
2016-10-14 01:26:59 -0500

    Redesigned Editor
    The editor will now be a single interface at the bottom of the
    container, and is known as the controller. The reason for the change is
    to simplify and remove the clutter from the old interface where every
    field’s unit had an input element, potentially hurting performance.

--- dynamizer.js
+++ dynamizer.js
@@ -14,80 +14,86 @@ dynamize([ interval [, setter ]]) : return
 
 			
 dynamize([ stop ])
-	- stop: A value of 'false' to stop updating, does not accept 'true'.
+	- stop: A boolean value, 'false' to stop updating, 'true' to start/restart.
 */
 jQuery.fn.dynamize = function() {
 	var interval	=(typeof arguments[0] == "number"? 		arguments[0] : 60) * 1000;
 	var setter 		= typeof arguments[1] == "function"? 	arguments[1] : 
 					  typeof arguments[0] == "function"? 	arguments[0] : false;
 					
-	var $root = $(this);
-	var $container = $($root.find("[data-body]")[0] || $root.find("tbody")[0] || this);
-	var $elementTemplate = $container.find("template");
+	var $containerRoot = $(this);
+	var $containerBody = $($containerRoot.find("[data-body]")[0] || $containerRoot.find("tbody")[0] || this);
+	var $containerItemTemplate = $containerBody.find("template");
+	var DataControl = $containerRoot.attr("data-control") != null;
 	
-	if ($root.data("updaterID")) clearInterval($root.data("updaterID"));
-	if (arguments[0] == false || !$root.attr("data-url") || !$elementTemplate) return;
+	if ($containerRoot.data("updaterID")) clearInterval($containerRoot.data("updaterID"));
+	if (arguments[0] == false || !$containerRoot.attr("data-url") || !$containerItemTemplate) return;
 	
-	
-	function configureControl($unit) {
-		return $unit.is("td, th, h1, h2, h3, h4, h5, h6")? (function() {
-			var $controller = $("<input>").attr("type", "text").addClass("form-control");
-			$unit.append($controller);
-			return $controller;
-		})() : $unit.is("p")? (function() {
-			var $controller = $("<textarea>").attr("rows", "4").addClass("form-control");
-			$unit.append($controller);
-			return $controller;
-		})() : $unit;
-	}
-	
-	function configure($element) {
-		// Assure fields for tables
-		if ($root.is("table")) $root.find("thead>tr>th").each(function(i, field) {
-			var child = $element.children()[i];			
-			var $cell = $(child || "<td>"); // Get template cell or make one
-			if (!$cell.attr("data-field")) $cell.attr("data-field", $(field).text());
-			if (!child) $element.append($cell);
-		});
-		
-		// Setup controls if required
-		if ($root.attr("data-control") != null) $element.find("[data-field]").each(function(i, unit) {
-			var $controller = configureControl($(unit));
-			if ($controller[0] !== unit) {
-				$controller.attr("data-field", $(unit).attr("data-field"));
-				$(unit).removeAttr("data-field");
-			}
-		});
-		
-		return $element;
-	}
-	
-	function update() {
-		$.getJSON($root.attr("data-url"), function(contents) {
+	function RequestUpdatedData() {
+		return $.getJSON($containerRoot.attr("data-url"), function(contents) {
 			
 			$.each(contents, function(i, content) {
-				var $element = $($container.children()[i+1] || configure($($elementTemplate.html())));				
+				var $element = $($containerBody.children()[i+1] || $MakeContainerItem());				
 				$element.find("[data-field]").each(function(j, unit) {
 					var field = $(unit).attr("data-field");
 					var value = content[field] || "[N/A]";
-					if (!setter || setter(field, value, content, $(unit), $element)) {
-						if ($(unit).is("input")) $(unit).attr("placeholder", value);
-						else $(unit).text(value);
-					}
+					if (!setter || setter(field, value, content, $(unit), $element)) $(unit).text(value);
 				});
-				if (!$element.parent().length) $container.append($element);
+				if (!$element.parent().length) $containerBody.append($element);
 			});
 			
-			$container.children(":gt(" + contents.length + ")").remove();
+			$containerBody.children(":gt(" + contents.length + ")").remove();
 			
 		}).fail(function(event) {
-			if (!$root.attr("data-url")) clearInterval($root.data("upaterID"));
+			if (!$containerRoot.attr("data-url")) clearInterval($containerRoot.data("upaterID"));
 			console.log("Failed to fetch! Check your server or the URL!");
 		});
 	}
 	
-	update(); // Launch it once, and schedule it.
-	$root.data("updaterID", setInterval(update, interval));
+	function $ContainerItemAddEditor($containerItem) {
+		var $button = $("<button>").attr("type", "button").addClass("btn btn-info btn-block").click(function() {
+			console.log("Edit!");
+		});
+		//$button.html("<span class='glyphicon glyphicon-edit'><span>");
+		$button.text("Edit");
+		$containerItem.append($containerItem.is("tr")? $("<td>").append($button) : $button);
+	}
+
+	function $ContainerItemConfigure($containerItem) {
+		// Assure fields for tables
+		if ($containerRoot.is("table")) $containerRoot.find("thead>tr>th").each(function(i, field) {
+			var child = $containerItem.children()[i];			
+			var $cell = $(child || "<td>"); // Get template cell or make one
+			if (!$cell.attr("data-field")) $cell.attr("data-field", $(field).text());
+			if (!child) $containerItem.append($cell);
+		});
+		
+		// Setup controls if required
+		if (DataControl) $ContainerItemAddEditor($containerItem);
+		
+		return $containerItem;
+	}
+
+	function $MakeContainerItem() {return $ContainerItemConfigure($($containerItemTemplate.html()));}
+	
+	// Populate the fields data array, if needed.
+	if (DataControl) (function() {
+		var $controllerRoot = $("<table>").addClass("table");
+		var $controllerHead = $("<thead>").append("<tr><th>Field</th><th>Value</th></tr>").appendTo($controllerRoot)
+		var $controllerBody = $("<tbody>").appendTo($controllerRoot);
+		$MakeContainerItem().find("[data-field]").each(function(i, unit) {
+			var fieldID = $(unit).attr("data-field");
+			var $fieldRow = $("<tr>");
+			var $fieldCell = $("<th>").attr("scope", "row").text(fieldID).appendTo($fieldRow);
+			var $valueCell = $("<td>").appendTo($fieldRow);
+			var $input = $("<input>").addClass("form-control").attr("type", "text").attr("data-field", fieldID).appendTo($valueCell);
+			$controllerBody.append($fieldRow);
+		});
+		$controllerRoot.insertAfter($containerRoot);
+	})();
+	
+	RequestUpdatedData(); // Launch it once, and schedule it.
+	$containerRoot.data("updaterID", setInterval(RequestUpdatedData, interval));
 	
 	return $(this);
 }
@@ -96,7 +102,7 @@ $(function() {
 	$("table.dynamic-schedule").dynamize(1, function(field, value, content, $unit, $element) {
 		if (field != "Date") return true;
 		
-		$unit.attr("placeholder", content["Time"]);
+		$unit.text(content["Time"]);
 		return false;
 	});
 	
